1b7a9b258d109c922f3417bdc2a9a20c1d26b383
MrZ <zhou953043100@qq.com>
2023-02-12 21:25:26 +0800

    coding

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: 大蒙
  * @Date: 2023-02-12 04:39:01
  * @LastEditors: 大蒙
- * @LastEditTime: 2023-02-12 21:23:49
+ * @LastEditTime: 2023-02-12 21:25:18
  * @FilePath: /vue3_vite_ts/src/App.vue
  * @Description: 
  * 
@@ -13,29 +13,29 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-<div>
-<a href="https://vitejs.dev" target="_blank">
-<img src="/vite.svg" class="logo" alt="Vite logo" />
-</a>
-<a href="https://vuejs.org/" target="_blank">
-<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-</a>
-</div>
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+    </div>
 
-<HelloWorld msg="Vite + Vue" />
+    <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
 .logo {
-height: 6em;
-padding: 1.5em;
-will-change: filter;
-transition: filter 300ms;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
 }
 .logo:hover {
-filter: drop-shadow(0 0 2em #646cffaa);
+    filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-filter: drop-shadow(0 0 2em #42b883aa);
+    filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
