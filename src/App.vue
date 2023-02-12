4269ed2fa5113db469a8edb18d4ec4c2514a0eeb
MrZ <zhou953043100@qq.com>
2023-02-12 21:30:42 +0800

    coding

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
     <div>
         <a href="https://vitejs.dev" target="_blank"> <img src="/vite.svg" class="logo" alt="Vite logo" /> </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
+        <a href="https://vuejs.org/" target="_blank"> <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" /> </a>11
     </div>
 
     <HelloWorld msg="Vite + Vue" />
